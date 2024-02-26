0ef657f0db2ae87b378ef87b1d5fa63eada7d8ed
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2024-02-26 16:24:19 -0300

    ajustes de config

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const config = {
     apiLocalUrl: 'http://localhost:5000',
+    //apiNews: 'https://newsapi.org/v2/top-headlines?country=br&apiKey=a4b4b67d75db430483b9abd07210adb9'
+    apiNews: 'https://newsapi.org/v2/top-headlines?country=br&apiKey=7fd36860f1a34555854fa040fdd48979'
   };
   
   export default config;
