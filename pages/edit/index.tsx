c2cb2a407aefb1afbed71f9b0b6e922065f518c5
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2024-03-01 12:26:49 -0300

    ajustes de pagina de edição

--- pages/edit/index.tsx
+++ pages/edit/index.tsx
@@ -2,6 +2,8 @@ import React, { useState, useRef } from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css"; // Importa o CSS do Quill
 import { Endpoints } from "../../src/app/components/endponts/endponts";
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
@@ -11,12 +13,13 @@ interface EditablePageProps {
 
 const EditablePage: React.FC<EditablePageProps> = ({ onSave }) => {
   const [content, setContent] = useState("");
-  const quillRef = useRef<any>();
+  const router = useRouter();
 
   const handleSave = async () => {
     try {
       const response = await Endpoints.salvarAviso(content);
       alert('Aviso salvo com sucesso:');
+      router.push("/");
     } catch (error) {
       console.error('Erro ao salvar o aviso:', error);
     }
@@ -25,7 +28,8 @@ const EditablePage: React.FC<EditablePageProps> = ({ onSave }) => {
   const modules = {
     toolbar: {
       container: [
-        [{ 'header': '1' }, { 'header': '2' }, { 'header': '3' }, { 'font': [] }],
+        [{ 'header': '1' }, { 'header': '2' }, { 'header': '3' }, { 'header': '4' }, { 'header': '5' }, { 'header': '6' }, { 'font': [] }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
         [{ 'color': [] }, { 'background': [] }],
         ['bold', 'italic', 'underline', 'strike'],
         [{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }, { 'align': 'justify' }],
@@ -35,15 +39,36 @@ const EditablePage: React.FC<EditablePageProps> = ({ onSave }) => {
     },
   };
 
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'color',
+    'background',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'align',
+    'link',
+    'image',
+    'clean',
+  ];
+
   return (
-    <div>
+    <div style={{ width: "800px", margin: "0 auto" }}> {/* Define o tamanho do editor */}
       <ReactQuill
         theme="snow"
         value={content}
         onChange={setContent}
         modules={modules}
+        formats={formats}
+        placeholder="Digite seu texto aqui..."
+        style={{ height: "500px", marginBottom: "20px" }} // Ajuste a altura e a margem inferior conforme necessário
       />
-      <button onClick={handleSave}>Salvar</button>
+      <Button variant="contained" color="primary" onClick={handleSave} style={{ marginBottom: "20px" }}>
+        Salvar
+      </Button>
     </div>
   );
 };
