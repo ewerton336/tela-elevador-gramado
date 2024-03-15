596027209ac5ba110f5ccc8a86b8465343e4fff8
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2024-03-15 09:29:23 -0300

    destruturação de dados no carousel de noticias

--- src/app/components/news/newsCarousel.tsx
+++ src/app/components/news/newsCarousel.tsx
@@ -28,8 +28,7 @@ const G1Carousel = () => {
       $(".feed-post-body").each((index, element) => {
         const title =
           $(element).find(".feed-post-link").text().trim() || "Sem título";
-        const link =
-          $(element).find(".feed-post-link").attr("href") || "#";
+        const link = $(element).find(".feed-post-link").attr("href") || "#";
         const description =
           $(element).find(".feed-post-body-resumo").text().trim() || "";
         const image =
@@ -45,117 +44,114 @@ const G1Carousel = () => {
   };
 
   useEffect(() => {
-    fetchArticles(); 
+    fetchArticles();
 
     const intervalId = setInterval(() => {
-      fetchArticles(); 
+      fetchArticles();
     }, config.FetchIntervalEndpontsExternos * 1000);
 
-    return () => clearInterval(intervalId); 
+    return () => clearInterval(intervalId);
   }, []);
 
-
-
-
   return (
-<Box
-  sx={{
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  }}
->
-  <Box
-    sx={{
-      maxWidth: "100%",
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      maxHeight: "100%",
-      overflow: "hidden",
-      "@media (max-width: 767px)": {
-        maxHeight: "none",
-        height: "auto",
-        marginBottom: "20px",
-      },
-    }}
-  >
-    <Swiper
-      modules={[Autoplay, Pagination]}
-      spaceBetween={30}
-      slidesPerView={1}
-      autoplay={{
-        delay: 10000,
-        disableOnInteraction: false,
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      pagination={{
-        clickable: true,
-      }}
-      loop={true}
     >
-      {articles
-        .filter((article) => article.title.length > 0 && article.image)
-        .map((article, index) => (
-          <SwiperSlide key={index}>
-            <Box
-              sx={{
-                position: "relative",
-                width: "100%",
-                height: "100%",
-                maxHeight: "500px",
-              }}
-            >
-              {article.image && (
+      <Box
+        sx={{
+          maxWidth: "100%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          maxHeight: "100%",
+          overflow: "hidden",
+          "@media (max-width: 767px)": {
+            maxHeight: "none",
+            height: "auto",
+            marginBottom: "20px",
+          },
+        }}
+      >
+        <Swiper
+          modules={[Autoplay, Pagination]}
+          spaceBetween={30}
+          slidesPerView={1}
+          autoplay={{
+            delay: 10000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          loop={true}
+        >
+          {articles
+            .filter((article) => article.title.length > 0 && article.image)
+            .map(({ title, image, description }, index) => (
+              <SwiperSlide key={index}>
                 <Box
-                  component="img"
                   sx={{
-                    objectFit: "cover",
-                    maxWidth: "100%",
+                    position: "relative",
                     width: "100%",
-                    height: "auto",
-                    borderRadius: "9px",
-                    margin: "auto",
-                    display: "block",
+                    height: "100%",
+                    maxHeight: "500px",
                   }}
-                  src={article.image}
-                  alt={article.title}
-                />
-              )}
-              <Box
-                sx={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "flex-end",
-                  padding: "20px",
-                  backgroundColor: "rgba(0, 0, 0, 0.5)",
-                  borderRadius: "9px",
-                  color: "#fff",
-                }}
-              >
-                <Typography variant="h4" textAlign="center" gutterBottom>
-                  {article.title}
-                </Typography>
-                <Typography variant="h4" textAlign="center">
-                  {article.description}
-                </Typography>
-                <Typography variant="h6" textAlign="center">
-                  Fonte: G1
-                </Typography>
-              </Box>
-            </Box>
-          </SwiperSlide>
-        ))}
-    </Swiper>
-  </Box>
-</Box>
+                >
+                  {image && (
+                    <Box
+                      component="img"
+                      sx={{
+                        objectFit: "cover",
+                        maxWidth: "100%",
+                        width: "100%",
+                        height: "auto",
+                        borderRadius: "9px",
+                        margin: "auto",
+                        display: "block",
+                      }}
+                      src={image}
+                      alt={title}
+                    />
+                  )}
+                  <Box
+                    sx={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-end",
+                      padding: "20px",
+                      backgroundColor: "rgba(0, 0, 0, 0.5)",
+                      borderRadius: "9px",
+                      color: "#fff",
+                    }}
+                  >
+                    <Typography variant="h4" textAlign="center" gutterBottom>
+                      {title}
+                    </Typography>
+                    <Typography variant="h4" textAlign="center">
+                      {description}
+                    </Typography>
+                    <Typography variant="h6" textAlign="center">
+                      Fonte: G1
+                    </Typography>
+                  </Box>
+                </Box>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </Box>
+    </Box>
   );
 };
 
