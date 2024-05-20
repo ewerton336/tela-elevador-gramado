b18238d1f6735ee49ed116ef5e9a9a92d04c33d0
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2024-05-20 14:01:27 -0300

    ajustes finos

--- src/app/components/avisos/avisos.tsx
+++ src/app/components/avisos/avisos.tsx
@@ -3,24 +3,29 @@ import {
   QueryClient,
   QueryClientProvider,
   useQuery,
-} from '@tanstack/react-query'
+} from "@tanstack/react-query";
 import { Endpoints } from "../endponts/endponts";
 import config from "../../../../config";
 
-
 const Avisos = () => {
-  const {isLoading, error, data: ultimoAviso } = useQuery({
-    queryKey: ['ultimoAviso'],
-    queryFn: () => Endpoints.buscarUltimo().then((response) => response.conteudo || ""),
+  const {
+    isLoading,
+    error,
+    data: ultimoAviso,
+  } = useQuery({
+    queryKey: ["ultimoAviso"],
+    queryFn: () =>
+      Endpoints.buscarUltimo().then((response) => response.conteudo || ""),
     refetchInterval: config.FetchIntervalAvisos * 1000,
   });
 
   if (isLoading) return <div>Carregando Avisos...</div>;
 
-  if (error) return <div>Ocorreu um erro ao carregar avisos: {error.message}</div>;
+  if (error)
+    return <div>Ocorreu um erro ao carregar avisos: {error.message}</div>;
 
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: "center" }}>
       <div dangerouslySetInnerHTML={{ __html: ultimoAviso }} />
     </div>
   );
