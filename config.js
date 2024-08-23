70d8a914291fd7e18f4e8e7b6ca3d1a30d4b64b2
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2024-08-23 16:19:23 -0300

    ajuste de novo endereço da api de avisos

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiLocalUrl: "http://51.79.39.238:5000",
+  apiLocalUrl: "http://38.46.142.216:5000",
   //apiNews: 'https://newsapi.org/v2/top-headlines?country=br&apiKey=a4b4b67d75db430483b9abd07210adb9'
   apiNews:
     "https://newsapi.org/v2/top-headlines?country=br&apiKey=7fd36860f1a34555854fa040fdd48979",
