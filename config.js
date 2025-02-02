e4cdf655fe49f7726a3b5c641907f317ffe6ff7f
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2025-02-02 10:24:24 -0300

    ajuste de yaml

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiLocalUrl: "http://38.46.142.216:5000",
+  apiLocalUrl: "http://51.79.39.251:5000",
   //apiNews: 'https://newsapi.org/v2/top-headlines?country=br&apiKey=a4b4b67d75db430483b9abd07210adb9'
   apiNews:
     "https://newsapi.org/v2/top-headlines?country=br&apiKey=7fd36860f1a34555854fa040fdd48979",
-    FetchIntervalEndpontsExternos: 300, // temmpo em segundos
-    FetchIntervalAvisos: 10, // temmpo em segundos
+  FetchIntervalEndpontsExternos: 300, // temmpo em segundos
+  FetchIntervalAvisos: 10, // temmpo em segundos
 };
 
 export default config;
